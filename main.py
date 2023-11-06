574730dab75294d457995ed63024e37a86a8d307
Barney Dono <sovietspy2@users.noreply.github.com>
2023-11-06 14:51:05 +0100

    adding some basic examples

--- main.py
+++ main.py
@@ -1,8 +1,120 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
+from enum import Enum
+from pydantic import BaseModel
+from typing import Annotated, List
 
 app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+#optional param example
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}
+
+#default param example
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+class Item(BaseModel):
+    name: str
+    description: str | None
+    price: float
+    tax: float | None
+
+@app.post("items2/")
+async def create_item(item: Item):
+    return item
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+#Fast api Query usage
+@app.get("/items/")
+async def read_items(
+    q: Annotated[
+        str | None, Query(min_length=3, max_length=50, pattern="^fixedquery$")
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/mandatory/")
+async def mandatory_param_example(param: Annotated[str, Query(min_length=1)] = ...):
+    print(param)
+    return "hell yeah"
+
+@app.get("/items/")
+async def read_items(q: Annotated[list, Query()] = []):
+    query_items = {"q": q}
+    return query_items
+
+# list with type check, all items must be int
+@app.get("/items3/")
+async def read_items3(q: Annotated[List[str], Query()] = ["foo", "bar"]):
+    query_items = {"q": q}
+    return query_items
+
+# query description example
+@app.get("/items4/")
+async def read_items4(
+    q: Annotated[
+        str | None,
+        Query(
+            title="Query string",
+            description="Query string for the items to search in the database that have a good match",
+            min_length=3,
+        ),
+    ] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+#non valid python param parsing with "alias parameter"
+@app.get("/items5/")
+async def read_items5(q: Annotated[str | None, Query(alias="item-query")] = None):
+    return "good"
+
+@app.get("/items6/{item_id}")
+async def read_items6(q: str, item_id: Annotated[int, Path(title="The ID of the item to get")]):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
